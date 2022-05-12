1abf9ae875f7c47c9e497765e9136f72e4b0090a
Kevin Rojas <kevintrevor0905@gmail.com>
2022-05-11 23:54:14 -0400

    Ejercicio de biseccion.

--- kevin_rojas_biseccion.py
+++ kevin_rojas_biseccion.py
@@ -22,13 +22,13 @@ def biseccion(fx, inter, er, n):
     Debe mostrar por teclado una a una las iteraciones del algoritmo de
     bisección,  hacer la comprobación. y retornar el último punto medio (m)
     """
-    Ea = 100 # Error aproximado
-    iter = 1 # Contador del número de iteraciones
+    err_aprox = 100 # Error aproximado
+    iter_act = 1 # Contador del número de iteraciones
     m_actual = 0 # Punto medio actual
     m_previo = 0 # Punto medio previo
     a = inter[0]
     b = inter[1]
-    while iter < n and Ea > er:
+    while iter_act < n and err_aprox > er:
         m_previo = m_actual
         m_actual = (a + b) / 2
 
@@ -37,13 +37,13 @@ def biseccion(fx, inter, er, n):
         else:
             b = m_actual
         
-        if iter > 1:
-            Ea = error_relativo(m_actual, m_previo)
+        if iter_act > 1:
+            err_aprox = error_relativo(m_actual, m_previo)
         
-        print(f"""===============Iteración {iter}===================
+        print(f"""===============Iteración {iter_act}===================
 Valor actual: {m_actual}
-Error relativo: {Ea}""")
-        iter += 1
+Error aproximado: {err_aprox}""")
+        iter_act += 1
     return m_actual
 
 
