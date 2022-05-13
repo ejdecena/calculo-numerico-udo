7f499bb851dddb76cba141af9702a615f883f3fb
Jeremy Gonzalez <jeremygonzalezg@gmail.com>
2022-05-13 16:12:43 -0400

    Ejercicio de biseccion.

--- test_ejercicios.py
+++ test_ejercicios.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 from . ejercicio_biseccion import biseccion as profe_biseccion
 # INCLUIR ABAJO SU IMPORT
-from . jeremy_gonzalez_biseccion import biseccion as pedro_perez_biseccion
+from . jeremy_gonzalez_biseccion import biseccion as jeremy_gonzalez_biseccion 
 
 
 class TestEjercicios(TestCase):
