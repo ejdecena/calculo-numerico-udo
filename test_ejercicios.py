f81ac4e8c0801313a88f35109276cfe85361d2a6
MegacesarCG <79388439+MegacesarCG@users.noreply.github.com>
2022-05-15 18:51:57 -0400

    Update test_ejercicios.py

--- test_ejercicios.py
+++ test_ejercicios.py
@@ -4,15 +4,10 @@ from unittest import TestCase
 from ejercicio_biseccion import biseccion as profe_biseccion
 from kevin_rojas_biseccion import biseccion as kevin_rojas_biseccion
 # INCLUIR ABAJO SU IMPORT
-<<<<<<< HEAD
-# from pedro_perez_biseccion import biseccion as pedro_perez_biseccion
 from jesus_gonzalez_biseccion import biseccion as jesus_gonzalez_biseccion
 from cesar_albornoz_biseccion import biseccion as cesar_albornoz_biseccion
-=======
-# from . pedro_perez_biseccion import biseccion as pedro_perez_biseccion
 from cesar_zabala_biseccion import biseccion as cesar_zabala_biseccion
 
->>>>>>> f1413f1 (Mi algoritmo de biseccion. Por favor que esto funcione x_x)
 
 class TestEjercicios(TestCase):
 
