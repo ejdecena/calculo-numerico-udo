9d491e1f6e87bf9523b1e7948b0bc839955b8631
Duffy <duffyalternativo@gmail.com>
2022-05-16 16:23:19 -0400

    Ejercicio de biseccion.

--- test_ejercicios.py
+++ test_ejercicios.py
@@ -1,8 +1,9 @@
 import math
 from unittest import TestCase
 
-#from . ejercicio_biseccion import biseccion as profe_biseccion
+from . ejercicio_biseccion import biseccion as profe_biseccion
 # INCLUIR ABAJO SU IMPORT
+#from . pedro_perez_biseccion import biseccion as pedro_perez_biseccion
 from . Duberth_Farias_biseccion import biseccion as pedro_perez_biseccion
 
 
@@ -13,6 +14,7 @@ class TestEjercicios(TestCase):
         self.inter = (1, 2)
         self.er = 0.02
         self.n = 50
+
 
 if __name__ == "__main__":
     # Testing.
