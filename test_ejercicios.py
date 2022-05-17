776c8cf43e8d3ac687d5674d7f5a7e0a5e26a87d
Diego Diaz <diegoandreeees@gmail.com>
2022-05-16 20:33:34 -0400

    prueba ultima

--- test_ejercicios.py
+++ test_ejercicios.py
@@ -9,6 +9,7 @@ from jesus_gonzalez_biseccion import biseccion as jesus_gonzalez_biseccion
 from cesar_albornoz_biseccion import biseccion as cesar_albornoz_biseccion
 from cesar_zabala_biseccion import biseccion as cesar_zabala_biseccion
 from nestor_aguilar_biseccion import biseccion as nestor_aguilar_biseccion
+from diego_diaz_biseccion import biseccion as diego_diaz_biseccion
 
 class TestEjercicios(TestCase):
 
