40bcbe737171b050fd29370a6a67b43ad168f0ab
UlisesHernandez Mora <ulisesjhm@gmail.com>
2022-05-17 13:19:57 -0400

    Ejercicio de biseccion.

--- test_ejercicios.py
+++ test_ejercicios.py
@@ -10,6 +10,7 @@ from cesar_albornoz_biseccion import biseccion as cesar_albornoz_biseccion
 from cesar_zabala_biseccion import biseccion as cesar_zabala_biseccion
 from nestor_aguilar_biseccion import biseccion as nestor_aguilar_biseccion
 from Omar_Gaspar_biseccion import biseccion as Omar_Gaspar_biseccion
+from ulises_hernandez_biseccion import biseccion as ulises_hernandez_biseccion
 
 class TestEjercicios(TestCase):
 
